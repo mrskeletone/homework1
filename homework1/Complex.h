a44cec9349e6c7246a72e3cfa6e1d6bec959de52
mrskeletone <mr_skeletone_1000@mail.ru>
2022-04-24 20:34:14 +0300

    1

---
+++ homework1/Complex.h
@@ -0,0 +1,16 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <iostream>
+#include <math.h>
+using namespace std;
+struct  Complex
+{
+	double im = 0;
+	double re = 0;
+};
+void sum(Complex n1, Complex n2);
+void vich(Complex n1, Complex n2);
+void modul(Complex n1);
+void ym(Complex n1, Complex n2);
+void del(Complex n1, Complex n2);
+#endif // !COMPLEX_H
---
+++ homework1/homework1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include "complex.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout << "Если хотитет сложить введите 1,вычесть 2,умножение 3, деление 4 ,модуль 5";
+    cout << endl;
+    int q;
+    cin >> q;
+    Complex a{ 5,3 };
+    Complex b{ -7,9 };
+    switch (q)
+    {
+    case 1: {
+
+        sum(a, b);
+        break; }
+    case 2: {
+        vich(a, b);
+        break; }
+    case 3: {
+
+        ym(a, b);
+        break; }
+    case 4: {
+
+        del(a, b);
+        break; }
+    case 5: {
+
+        modul(a);
+        break; }
+    }
+
+    return 0;
+}
